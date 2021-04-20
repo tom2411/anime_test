30ab200a9d6bf0275f234186c8acd28e7fb61d11
Tom Ribardiere <tom.ribardiere@free.fr>
2021-04-20 14:32:13 +0200

    Initial commit

---
+++ src/main/java/com/example/anime_test/AnimeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.anime_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeTestApplication.class, args);
+    }
+
+}
